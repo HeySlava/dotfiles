6c0e8a02b21b22416b3b7d727365b3545872effa
HeySlava <69298864+HeySlava@users.noreply.github.com>
2022-08-07 13:12:22 +0300

    delete unneeded require

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,1 @@
 require('slava')
---
---
--- require('packer-config')
-
--- require('settings')
--- require('mapping')
--- require('markdown')
--- require('ultisnip')
--- require('xkbswitch')
-
--- require('telescope-config')
-
--- require('lsp-config.cmp-settings')
--- require('lsp-config.language-servers')
