4ede3197ddd307620a2af250831487f769a9c350
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-04-16 16:04:23 +0300

    moved autocommand into setttings module

--- nvim/.config/nvim/lua/slava/init.lua
+++ nvim/.config/nvim/lua/slava/init.lua
@@ -4,14 +4,3 @@ require('slava.lsp-config')
 require('slava.settings')
 require('slava.mapping')
 require('slava.markdown')
-
-
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = '*.py',
-    command = 'set colorcolumn=88'
-})
-
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = '*.arch',
-    command = 'UltiSnipsAddFiletypes arch'
-})
--- nvim/.config/nvim/lua/slava/settings.lua
+++ nvim/.config/nvim/lua/slava/settings.lua
@@ -38,3 +38,20 @@ opt.undofile = true
 g.netrw_preview   = 1
 g.netrw_liststyle = 3
 g.netrw_winsize   = 30
+
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.py',
+    command = 'set colorcolumn=88'
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.arch',
+    command = 'UltiSnipsAddFiletypes arch'
+})
+
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.git/COMMIT_EDITMSG',
+    command = 'set colorcolumn=50,72'
+})
